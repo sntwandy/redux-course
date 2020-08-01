bb6e8bfd46a8b789685da94d3220d662140fba46
sntwandy <sntwandy@gmail.com>
2020-07-31 23:47:45 -0400

    Table is created

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,33 @@
 import React from 'react';
 
+// Styles
+import '../assets/styles/containers/App.scss';
+
 const App = () => {
   return(
-    <h1>Hello!!</h1>
+    <div className="margin">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Megan</td>
+            <td>hrramegan@gmail.com</td>
+            <td>meganherrera.com</td>
+          </tr>
+          <tr>
+            <td>Wandy</td>
+            <td>sntwandy@gmail.com</td>
+            <td>sntwandy.com</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -4,4 +4,7 @@ import ReactDOM from 'react-dom';
 // Component
 import App from './containers/App';
 
+// Global /style
+import './assets/styles/Global.scss';
+
 ReactDOM.render(<App />, document.querySelector('.App'));
