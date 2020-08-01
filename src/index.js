b6f1b1f045d3a0d1769bcd2fbe5f89de68bf6dbb
sntwandy <sntwandy@gmail.com>
2020-08-01 16:15:24 -0400

    Creating store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'; // eslint-disable-line
 
 // Component
 import App from './containers/App';
 
 // Global /style
 import './assets/styles/Global.scss';
+
+const store = createStore(); // eslint-disable-line
 
 ReactDOM.render(<App />, document.querySelector('.App'));
