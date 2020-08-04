8eab84fc381b72e2b619087aae771222ea984d48
sntwandy <sntwandy@gmail.com>
2020-08-04 16:35:46 -0400

    Adding support to images statics to webpack and SSR, home new design

--- src/frontend/containers/Home.js
+++ src/frontend/containers/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+// Assets
+import blogimg from '../assets/static/blog-landing.png';
+
+// Styles
+import '../assets/styles/containers/Home.scss';
+
 const Home = () => {
   return(
     <>
-      <h2>Home</h2>
+    <div className="home-container">
+      <h2>Welcome to the Blog!</h2>
+      <div className="blogimg-container">
+        <img src={blogimg} alt="Blog"/>
+      </div>
+    </div>
     </>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -73,6 +73,17 @@ module.exports = {
           'sass-loader',
         ],
       },
+      {
+        test: /\.(png|gif|jpg)$/,
+        use: [
+            {
+                loader: 'file-loader',
+                options: {
+                    name: 'assets/[name].[ext]'
+                }
+            }
+        ]
+    }
     ],
   },
   plugins: [
